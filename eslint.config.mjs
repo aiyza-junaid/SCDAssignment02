f82cf26e952ea0e4b0a97718eaa66f90898d913e
aiyza-junaid <aiyza.junaid@gmail.com>
2024-04-24 23:48:03 +0500

    kklm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,14 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 // Define ESLint configuration
-export default [
+const config = [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
+
+// Polyfill optional chaining and nullish coalescing for environments that don't support them
+if (!config[0].languageOptions) config[0].languageOptions = {};
+config[0].languageOptions.ecmaVersion = 2021;
+
+export default config;
