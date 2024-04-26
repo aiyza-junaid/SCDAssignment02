170bf39d7ab40aab74b535989c3d2e9c56711d57
aiyza-junaid <aiyza.junaid@gmail.com>
2024-04-26 21:46:36 +0500

    assignment02

--- .test.js
+++ .test.js
@@ -1,14 +1,14 @@
-const { getTimestamp, md5 } = require('./src/utils'); // Adjust the path if necessary
+const { getTimestamp, md5 } = require('./src/utils');
 
 describe('Utility Functions', () => {
-  // Test case for getTimestamp function
+  //Test case for getTimestamp function
   test('getTimestamp converts time string to seconds', () => {
     const time = '12:30:15';
     const expectedSeconds = 12 * 3600 + 30 * 60 + 15;
     expect(getTimestamp(time)).toBe(expectedSeconds);
   });
 
-  // Test case for md5 function
+  //Test case for md5 function
   test('md5 calculates the MD5 hash of a string', () => {
     const inputString = 'hello';
     const expectedHash = '5d41402abc4b2a76b9719d911017c592';
